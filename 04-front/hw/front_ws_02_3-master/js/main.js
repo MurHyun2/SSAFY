efb3c38bc523f528bb50df76d85e1437a5f2ae0f
이대현 <eddy1219@naver.com>
2024-08-22 17:54:49 +0900

    2024-08-22 ssafy2

---
+++ 04-front/hw/front_ws_02_3-master/js/main.js
@@ -0,0 +1,11 @@
+const btn = document.querySelectorAll(".button");
+console.log(btn);
+
+btn.onclick = function () {
+  const title = this.document.querySelector(".title");
+  const genre = this.document.querySelector(".genre");
+  const director = this.document.querySelector(".director");
+  const runningTime = this.document.querySelector(".runningTime");
+  
+  console.log(title);
+};
