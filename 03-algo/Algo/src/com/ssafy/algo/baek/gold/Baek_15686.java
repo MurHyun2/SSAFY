cc6e0cb2d18ccb25ca9afb7b65aecc4b72f195dc
이대현 <eddy1219@naver.com>
2024-09-03 00:07:36 +0900

    2024-09-02 home

--- 03-algo/Algo/src/com/ssafy/algo/baek/gold/Baek_15686.java
+++ 03-algo/Algo/src/com/ssafy/algo/baek/gold/Baek_15686.java
@@ -3,12 +3,27 @@ package com.ssafy.algo.baek.gold;
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
+import java.util.ArrayList;
+import java.util.List;
 import java.util.StringTokenizer;
 
 public class Baek_15686 {
 
+	static class loc {
+		int x, y;
+
+		public loc(int x, int y) {
+			super();
+			this.x = x;
+			this.y = y;
+		}
+	}
+
 	static int N, M, min;
 	static int[][] arr;
+	static List<loc> chicken = new ArrayList<>();
+	static List<loc> house = new ArrayList<>();
+	static boolean[] open;
 
 	public static void main(String[] args) throws IOException {
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
@@ -23,25 +38,51 @@ public class Baek_15686 {
 
 			for (int c = 0; c < N; c++) {
 				arr[r][c] = Integer.parseInt(st.nextToken());
+
+				if (arr[r][c] == 1) {
+					house.add(new loc(r, c));
+				} else if (arr[r][c] == 2) {
+					chicken.add(new loc(r, c));
+				}
 			}
 		}
 		// 입력 끝
 
-		min = 0;
+		min = Integer.MAX_VALUE;
+		open = new boolean[chicken.size()];
 
-		bt(0, 0);
+		comb(0, 0);
 
+		System.out.println(min);
 	}
 
-	private static void bt(int now, int depth) {
-		int sum = 0;
+	private static void comb(int now, int depth) {
+		if (depth == M) {
+			int sum = 0;
 
-		for (int r = 0; r < N; r++) {
-			for (int c = 0; c < N; c++) {
-				if (arr[r][c] == 1) {
-					
+			for (int i = 0; i < house.size(); i++) {
+				int minDis = Integer.MAX_VALUE;
+
+				for (int j = 0; j < chicken.size(); j++) {
+					if (open[j]) {
+						int dis = Math.abs(house.get(i).x - chicken.get(j).x)
+								+ Math.abs(house.get(i).y - chicken.get(j).y);
+
+						minDis = Math.min(minDis, dis);
+					}
 				}
+
+				sum += minDis;
 			}
+
+			min = Math.min(min, sum);
+			return;
+		}
+
+		for (int i = now; i < chicken.size(); i++) {
+			open[i] = true;
+			comb(i + 1, depth + 1);
+			open[i] = false;
 		}
 	}
 }
---
+++ 03-algo/Algo/src/com/ssafy/algo/baek/gold/qwe.java
@@ -0,0 +1,5 @@
+package com.ssafy.algo.baek.gold;
+
+public class qwe {
+
+}
